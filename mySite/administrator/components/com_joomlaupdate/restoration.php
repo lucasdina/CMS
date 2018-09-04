d5e56441fc72e6a3e82007ffb1527b4ad91b7705
Lucas Dina <ldina@SWDV.dom>
2018-09-03 20:34:14 -0600

    Adding db backup.

---
+++ mySite/administrator/components/com_joomlaupdate/restoration.php
@@ -0,0 +1,15 @@
+<?php
+defined('_AKEEBA_RESTORATION') or die('Restricted access');
+$restoration_setup = array(
+	'kickstart.security.password' => 'Z4YGnUdjITJUMtCtKehBGqUJfuesio48',
+	'kickstart.tuning.max_exec_time' => '5',
+	'kickstart.tuning.run_time_bias' => '75',
+	'kickstart.tuning.min_exec_time' => '0',
+	'kickstart.procengine' => 'direct',
+	'kickstart.setup.sourcefile' => 'C:\Users\ldina\code\CMS\mySite/tmp/',
+	'kickstart.setup.destdir' => 'C:\Users\ldina\code\CMS\mySite',
+	'kickstart.setup.restoreperms' => '0',
+	'kickstart.setup.filetype' => 'zip',
+	'kickstart.setup.dryrun' => '0',
+	'kickstart.setup.renamefiles' => array(),
+	'kickstart.setup.postrenamefiles' => false);
---
+++ mySite/administrator/logs/joomla_update.php
@@ -0,0 +1,16 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-09-04 02:22:00 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-09-04T02:22:00+00:00	INFO 172.22.0.1	update	Update started by user Super User (631). Old version is 3.8.11.
+2018-09-04T02:22:02+00:00	INFO 172.22.0.1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.8.12-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20180904%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180904T022157Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=285dcfc15268907db0d52f6db8167d7421c3a97eab5e57de35064e489affe851.
+2018-09-04T02:22:03+00:00	INFO 172.22.0.1	update	File Joomla_3.8.12-Stable-Update_Package.zip downloaded.
+2018-09-04T02:22:03+00:00	INFO 172.22.0.1	update	Starting installation of new version.
+2018-09-04T02:22:22+00:00	INFO 172.22.0.1	update	Finalising installation.
+2018-09-04T02:22:22+00:00	INFO 172.22.0.1	update	Deleting removed files and folders.
+2018-09-04T02:22:25+00:00	INFO 172.22.0.1	update	Cleaning up after installation.
+2018-09-04T02:22:25+00:00	INFO 172.22.0.1	update	Update to version 3.8.12 is complete.
+2018-09-04T02:23:50+00:00	INFO 172.22.0.1	update	Starting installation of new version.
+2018-09-04T02:23:58+00:00	INFO 172.22.0.1	update	Starting installation of new version.
