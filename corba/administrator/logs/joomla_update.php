22fdd226fd01404862142c2d4241e5780dd13e29
Lucas Dina <ldina@SWDV.dom>
2018-09-09 21:34:05 -0600

    Adding db backup.

---
+++ corba/administrator/logs/joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-09-10 03:26:40 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-09-10T03:26:40+00:00	INFO ::1	update	Update started by user Super User (262). Old version is 3.8.11.
+2018-09-10T03:26:42+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.8.12-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20180910%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180910T032636Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=944b1081dcf11679cb1a0a70d0be15b9bbb9613a6fda9cb25006bb0a14bc37cc.
+2018-09-10T03:26:43+00:00	INFO ::1	update	File Joomla_3.8.12-Stable-Update_Package.zip downloaded.
+2018-09-10T03:26:43+00:00	INFO ::1	update	Starting installation of new version.
+2018-09-10T03:27:04+00:00	INFO ::1	update	Finalising installation.
+2018-09-10T03:27:04+00:00	INFO ::1	update	Deleting removed files and folders.
+2018-09-10T03:27:05+00:00	INFO ::1	update	Cleaning up after installation.
+2018-09-10T03:27:05+00:00	INFO ::1	update	Update to version 3.8.12 is complete.
