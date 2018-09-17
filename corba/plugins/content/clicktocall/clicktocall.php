6630d7f2afc43e4ffcc5a5f529aacf551381c403
Lucas Dina <ldina@SWDV.dom>
2018-09-17 16:34:02 -0600

    Adding db backup.

--- corba/plugins/content/clicktocall/clicktocall.php
+++ corba/plugins/content/clicktocall/clicktocall.php
@@ -3,7 +3,7 @@ defined( '_JEXEC' ) or die;
 
 class plgContentClickToCall extends JPlugin
 {
-	public function onContentPrepare($context, $article, $params, $limitstart) {
+	public function onContentAfterDisplay($context, $article, $params, $limitstart) {
         $regexA = "/\d?(\s?|-?|\+?|\.?)((\(\d{1,4}\))|(\d{1,3})|\s?)(\s?|-?|\.?)((\(\d{1,3}\))|(\d{1,3})|\s?)(\s?|-?|\.?)((\(\d{1,3}\))|(\d{1,3})|\s?)(\s?|-?|\.?)\d{3}(-|\.|\s)\d{4}/";
         $article->text = preg_replace($regexA, '<a href="tel:+$0">$0</a>', $article->text);
         return true;
--- corba/plugins/content/colorize/colorize.php
+++ corba/plugins/content/colorize/colorize.php
@@ -4,7 +4,7 @@ defined( '_JEXEC' ) or die;
 class plgContentColorize extends JPlugin
 {
 	public function onContentPrepare($context, $article, $params, $limitstart) {
-        $regexA = "/\b(a|an|the)\b/i";
+        $regexA = "/\b((?<!\<)a|an|the)\b/i";
         $article->text = preg_replace($regexA, '<font color="red">$0</font>', $article->text);
         return true;
     }
